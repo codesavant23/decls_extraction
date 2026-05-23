c90d13b26eb348c32dd2705517fb35f54167b142
Filippo Mecozzi <filippo.mecozzi00@gmail.com>
2026-05-23 17:46:51 +0200

    Add component docstring
    Added docstring to explain the purpose of the component.

--- src/decls_extraction/__init__.py
+++ src/decls_extraction/__init__.py
@@ -1,3 +1,12 @@
+"""
+Python autonomous component which gives the ability to extract:
+- Functions
+- Methods
+- Classes
+
+View the enumerator `ECodeParserTool` for the currently implemented, and available, parsers.
+"""
+
 from . import classdecls_extractor
 from . import moddecls_extractor
 
